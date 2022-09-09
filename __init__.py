81171e7fde77c01c72e4f0c16ea609bc8a1e7bdb
Deven Manam <deven.manam@gmail.com>
2022-09-08 20:23:10 -0500

    include hacky one-time-use functions for posterity

--- __init__.py
+++ __init__.py
@@ -67,6 +67,64 @@ def markUnique():
     dialog.setStandardButtons(QMessageBox.Ok)
     dialog.exec()
 
+def copyInfo():
+    decks = mw.addonManager.getConfig(__name__)['unsuspender']
+    num = 0
+    for deck_id in decks:
+        for row in mw.col.db.execute('SELECT nid FROM cards WHERE did = ?', deck_id):
+            note = mw.col.getNote(row[0]) # get_note
+            did = False
+            if note["Key"] != "":
+                note["Recognition"] = "true"
+                did |= True
+            if note["XHZ Meaning"] != "":
+                note["XHZ"] = "true"
+                did |= True
+            note.flush()
+            if did:
+                num += 1
+    dialog = QMessageBox()
+    dialog.setText("did %i" % num)
+    dialog.setStandardButtons(QMessageBox.Ok)
+    dialog.exec()
+
+def doMerge():
+    pairs = defaultdict(lambda: {"rec": None, "xhz": None, "skip": False})
+    toomany = []
+    toofew = []
+    toremove = []
+    for row0 in mw.col.db.execute('SELECT id FROM notes WHERE tags LIKE "% duplicate %"'):
+        for row in mw.col.db.execute('SELECT id, nid, did FROM cards WHERE nid = ?', row0[0]):
+            note = mw.col.getNote(row[1])
+            hanzi = note["Simplified"]
+            deck = "rec" if int(row[2]) == 1660023039388 else "xhz"
+            if pairs[hanzi][deck] != None:
+                pairs[hanzi]["skip"] = True
+                toomany += [hanzi]
+            else:
+                pairs[hanzi][deck] = row
+    for hanzi, pair in pairs.items():
+        rec = pair["rec"]
+        xhz = pair["xhz"]
+        if pair["skip"]:
+            continue
+        if not rec or not xhz:
+            toofew += [hanzi]
+            continue
+        recnote = mw.col.getNote(rec[1])
+        xhznote = mw.col.getNote(xhz[1])
+        for key in ["Key", "Traditional", "Pinyin", "Pinyin ASCII", "Meaning", "Part of speech", "Audio", "Homophone", "Homograph", "Sentence Simplified", "Sentence Traditional", "Sentence Simplified Cloze", "Sentence Traditional Cloze", "Sentence Pinyin", "Sentence Pinyin ASCII", "Sentence Meaning", "Sentence Audio", "Sentence Image"]:
+            xhznote[key] = recnote[key]
+        xhznote.tags += recnote.tags
+        mw.col.db.execute('UPDATE cards SET nid = ? WHERE id = ?', xhz[1], rec[0])
+        mw.col.db.execute("DELETE FROM notes WHERE id = ?", rec[1])
+        mw.col.db.execute("DELETE FROM cards WHERE nid = ?", rec[1])
+        xhznote.flush()
+    dialog = QMessageBox()
+    dialog.setText("Skipped " + str(toomany) + " for having too many and " + str(toofew) + " for having too few")
+    dialog.setStandardButtons(QMessageBox.Ok)
+    dialog.exec()
+
 menu = QMenu('Hanzi Helper', mw)
 menu_show_decks = menu.addAction('Show Deck IDs')
 menu_unsuspend = menu.addAction('Run Unsuspender')
